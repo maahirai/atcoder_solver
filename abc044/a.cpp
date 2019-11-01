3cdf53aa99e44f1a6e34413ffc6d170ff6c8f2e8
musuchika <musu.tec@gmail.com>
2019-11-01 22:59:32 +0900

    c,dわからんかった

---
+++ abc044/a.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<iomanip>
+#include<vector>
+#include<map>
+#include<set>
+#include<algorithm>
+#include<numeric>
+#include<limits>
+#include<bitset>
+#include<functional>
+#include<type_traits>
+#include<queue>
+#include<stack>
+#include<array>
+#include<random>
+#include<utility>
+#include<cstdlib>
+#include<ctime>
+
+using namespace std;
+
+template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b;return true;}return false;}
+template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b;return true;}return false;}
+
+#define rep(i,n) for(int i=0;i<(int)n;i++)
+#define rep2(i, begin_i, end_i) for (int i = (int)begin_i; i < (int)end_i; i++)
+
+long long INF = 1LL<<60;
+long long dp[100010];
+int h[100010];
+int n,k,x,y;
+int main(){
+    cin>>n>>k>>x>>y;
+    int i=0;
+    long long sum=0;
+    for(i;i<n;i++){
+    if(i<k)
+        sum+=x;
+    else
+        sum+=y;
+    }
+    cout<<sum<<endl;
+    return 0;
+}
+
+
---
+++ abc044/b.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<iomanip>
+#include<vector>
+#include<map>
+#include<set>
+#include<algorithm>
+#include<numeric>
+#include<limits>
+#include<bitset>
+#include<functional>
+#include<type_traits>
+#include<queue>
+#include<stack>
+#include<array>
+#include<random>
+#include<utility>
+#include<cstdlib>
+#include<ctime>
+
+using namespace std;
+
+template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b;return true;}return false;}
+template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b;return true;}return false;}
+
+#define rep(i,n) for(int i=0;i<(int)n;i++)
+#define rep2(i, begin_i, end_i) for (int i = (int)begin_i; i < (int)end_i; i++)
+
+long long INF = 1LL<<60;
+long long dp[100010];
+int h[100010];
+int main(){
+    string w;
+    cin>>w;
+    rep(i,w.size())
+    {
+        if(w.at(i)<'a'||w.at(i)>'z'){
+            cout<<"No"<<endl;
+            return 0;
+        }
+        h[w.at(i)-'a']++;
+    }
+    rep(i,26){
+    if(h[i]%2){
+        cout<<"No"<<endl;
+return 0;}
+    }
+    cout<<"Yes"<<endl;
+    return 0;
+}
+
+
