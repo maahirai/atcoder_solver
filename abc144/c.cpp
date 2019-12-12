dbad77d4bb0b7cf93ecded5b5be2decb43f4f130
musuchika <musu.tec@gmail.com>
2019-12-12 20:33:38 +0900

    桁溢れしないかチェック

---
+++ abc144/c.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<iomanip>
+#include<vector>
+#include<map>
+#include<set>
+#include<algorithm>
+#include<numeric>
+#include<limits>
+#include<bitset>
+#include<functional>
+#include<type_traits>
+#include<queue>
+#include<stack>
+#include<array>
+#include<random>
+#include<utility>
+#include<cstdlib>
+#include<ctime>
+
+using namespace std;
+
+typedef  long long ll;
+template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b;return true;}return false;}
+template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b;return true;}return false;}
+
+#define rep(i,n) for(int i=0;i<(int)n;i++)
+#define rep2(i, begin_i, end_i) for (int i = (int)begin_i; i < (int)end_i; i++)
+
+long long INF = 1LL<<60;
+long long dp[100010];
+int h[100010];
+int64_t n;
+int main(){
+    cin>>n;
+    int64_t x,y;
+    for(int64_t i=1;i<=sqrt(n);i++){
+        if(!(n%i))y=i;
+    }
+    x=n/y;
+    cout<<x+y-2<<endl;
+    return 0;
+}
+
+
