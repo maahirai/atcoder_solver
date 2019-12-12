e5dfd2ef7fe888bcdd3d2d3675eae696992cb4f4
musuchika <musu.tec@gmail.com>
2019-12-12 10:53:16 +0900

    12/12

---
+++ abc090/c.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<iomanip>
+#include<vector>
+#include<map>
+#include<set>
+#include<algorithm>
+#include<numeric>
+#include<limits>
+#include<bitset>
+#include<functional>
+#include<type_traits>
+#include<queue>
+#include<stack>
+#include<array>
+#include<random>
+#include<utility>
+#include<cstdlib>
+#include<ctime>
+
+using namespace std;
+
+typedef  long long ll;
+template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b;return true;}return false;}
+template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b;return true;}return false;}
+
+#define rep(i,n) for(int i=0;i<(int)n;i++)
+#define rep2(i, begin_i, end_i) for (int i = (int)begin_i; i < (int)end_i; i++)
+
+long long INF = 1LL<<60;
+long long dp[100010];
+int h[100010];
+int x;
+int main(){
+    int64_t n,m;
+    cin>>n>>m;
+    if(n==1&&m==1)cout<<1<<endl;
+    else if(m==1){cout<<n-2<<endl;}
+    else if(n==1){cout<<m-2<<endl;}
+    else cout<<(n-2)*(m-2)<<endl;
+    return 0;
+}
+
+
