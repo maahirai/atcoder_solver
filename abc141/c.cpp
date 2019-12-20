f4c44a602d17cff1d29f9e6bdd384ec2d4a32ddb
musuchika <musu.tec@gmail.com>
2019-12-20 18:14:27 +0900

    前にやった

---
+++ abc141/c.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef  long long ll;
+template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b;return true;}return false;}
+template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b;return true;}return false;}
+
+#define rep(i,n) for(int i=0;i<(int)n;i++)
+#define rep2(i, begin_i, end_i) for (int i = (int)begin_i; i < (int)end_i; i++)
+
+long long INF = 1LL<<60;
+long long dp[100010];
+int h[100010];
+ll n,k,q;
+int main(){
+    cin>>n>>k>>q;
+    vector<int> a(q);
+    vector<int> score(n);
+    rep(i,q) cin>>a.at(i);
+    rep(i,q) score.at(a.at(i)-1)++;
+    rep(i,n){if(score.at(i)+k>q)cout<<"Yes"<<endl;else cout<<"No"<<endl;}
+    return 0;
+}
+
+
