04f4eec6cd532f3b0e0548bc164773f55e816237
musuchika <musu.tec@gmail.com>
2019-12-21 14:03:43 +0900

    証明できないかも

---
+++ abc136/c.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef  long long ll;
+template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b;return true;}return false;}
+template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b;return true;}return false;}
+
+#define rep(i,n) for(int i=0;i<(int)n;i++)
+#define rep2(i, begin_i, end_i) for (int i = (int)begin_i; i < (int)end_i; i++)
+
+long long INF = 1LL<<60;
+long long dp[100010];
+int h[100010];
+int n;
+int main(){
+    cin>>n;
+    vector<int> h(n);
+    rep(i,n)cin>>h.at(i);
+    if(n==1){cout<<"Yes"<<endl;return 0;}
+    else{
+        int heightest=h.at(0);
+        rep(i,n){
+            if(h.at(i)<=(heightest-2)){
+                cout<<"No"<<endl;
+                return 0;
+            }
+            if(heightest<h.at(i))heightest=h.at(i);
+        }
+    }
+    cout<<"Yes"<<endl;
+    return 0;
+}
+
+
