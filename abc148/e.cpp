258ca5b472f24e277626ed2073f7498e5d46b0d8
musuchika <musu.tec@gmail.com>
2019-12-23 18:11:53 +0900

    WAの方にも書いた様にf(n)の値の素因分数f(n)=2^a*5^b*...のbが答え(a>bだから).したがって，bの値を求めたい．ここでnに注目すると，nが奇数の時はf(n)=0なので条件分岐で先に除く.nが偶数の時，f(n)の式に出てくる5^kの倍数(kは自然数)も偶数のみである．したがって，5^k*2で割れる値が式に出てくる回数を求めたら良いということに気づきたかった．勉強不足だったので精進する．

---
+++ abc148/e.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef  long long ll;
+template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b;return true;}return false;}
+template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b;return true;}return false;}
+
+#define rep(i,n) for(int i=0;i<(int)n;i++)
+#define rep2(i, begin_i, end_i) for (int i = (int)begin_i; i < (int)end_i; i++)
+ ll solve(ll n){
+        ll ans=0;
+        ll div=10;
+        while(div<=n){
+            ans+=n/div;
+            div*=5;
+        }
+        return ans;
+    }
+long long INF = 1LL<<60;
+int main(){
+    ll n;
+    cin>>n;
+    if(n%2){
+        cout<<0<<endl;
+    }
+    else{
+       cout<<solve(n)<<endl;
+    }
+    return 0;
+}
