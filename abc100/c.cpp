4f8d893442e9a33d7345f52efa7528dc823df8d1
musuchika <musu.tec@gmail.com>
2019-12-24 21:02:28 +0900

    ぱやぱや

---
+++ abc100/c.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef  long long ll;
+template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b;return true;}return false;}
+template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b;return true;}return false;}
+//自作関数
+template<class T> ll llpow(T x,T n){ll ans=1;while(n--){ans*=x;}return ans;}
+
+#define rep(i,n) for(ll i=0;i<(ll)n;i++)
+#define rep2(i, begin_i, end_i) for (ll i = (ll)begin_i; i < (ll)end_i; i++)
+
+long long INF = 1LL<<60;
+int n;
+int main(){
+    cin>>n;
+    vector<int> a(n);
+    rep(i,n)cin>>a.at(i);
+    int countdiv2=0;
+    for(int i=0;i<n;i++){
+        while(!(a.at(i)%2)){
+            a.at(i)/=2;
+            countdiv2++;
+        }
+    }
+    cout<<countdiv2<<endl;
+    return 0;
+}
+
+
