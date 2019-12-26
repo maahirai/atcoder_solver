06b31e9d817a2d588772810b3e9e2711e55e190b
musuchika <musu.tec@gmail.com>
2019-12-26 18:53:10 +0900

    全てのtにおいて,進むことを選択したときに\sum_{k=1}^{t} kまで進むことができる．ここで，数学的帰納法でtまでに行った選択のうち，適当なものを現在位置に位置にとどまる選択にすることで0から\sum_{k=1}^{t} kまでの全てのマスに時刻tの時点で居られることを証明した．これによって，気にすべきことは\sum_{k=1}^{t} kがXを超えるかどうかだけになる

---
+++ abc056/c.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef  long long ll;
+template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b;return true;}return false;}
+template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b;return true;}return false;}
+//自作関数
+template<class T> ll llpow(T x,T n){ll ans=1;while(n--){ans*=x;}return ans;}
+
+#define rep(i,n) for(ll i=0;i<(ll)n;i++)
+#define rep2(i, begin_i, end_i) for (ll i = (ll)begin_i; i < (ll)end_i; i++)
+
+long long INF = 1LL<<60;
+int n;
+int main(){
+    ll x;
+    cin>>x;
+    ll edge=0;
+    ll ans=0;
+    for(ll i=0;edge<x;i++){
+        edge+=i;
+        ans=i;
+    }
+    cout<<ans<<endl;
+    return 0;
+}
+
+
