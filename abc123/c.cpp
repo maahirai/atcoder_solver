7f561f48abf5f2bb56e1f0eb8703b5286888b18c
musuchika <musu.tec@gmail.com>
2020-01-10 11:11:33 +0900

    やり直した

--- abc123/c.cpp
+++ abc123/c.cpp
@@ -10,21 +10,17 @@ template<class T> ll llpow(T x,T n){ll ans=1;while(n--){ans*=x;}return ans;}
 #define rep2(i, begin_i, end_i) for (ll i = (ll)begin_i; i < (ll)end_i; i++)
 
 long long INF = 1LL<<60;
+int n;
 int main(){
-    ll n,sekisai[5];
+    ll n;
     cin>>n;
-    rep(i,5)cin>>sekisai[i];
-    ll hito[6]={n,0,0,0,0,0};
-    ll time=0;
-    ll saiakunoeki=0;
-    ll saiaku=1e15;
-    rep(i,5){
-    if(saiaku>sekisai[i]){
-        saiakunoeki=i;
-        saiaku=sekisai[i];
-        }
+    ll canride[5];
+    rep(i,5)cin>>canride[i];
+    int mini=0;
+    rep(i,5)if(canride[i]<canride[mini]){
+        mini=i;
     }
-    cout<<(n%min({sekisai[0],sekisai[1],sekisai[2],sekisai[3],sekisai[4]})!=0)+n/min({sekisai[0],sekisai[1],sekisai[2],sekisai[3],sekisai[4]})+4<<endl;
+    cout<<4+(n+canride[mini]-1)/canride[mini]<<endl;
     return 0;
 }
 
