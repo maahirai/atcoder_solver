248de8e645c06a2d97b859e4ebacf06b89c7f904
musuchika <musu.tec@gmail.com>
2020-02-01 16:32:36 +0900

    再帰書けない

---
+++ abc153/d_.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef  long long ll;
+template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b;return true;}return false;}
+template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b;return true;}return false;}
+//自作関数
+template<class T> ll llpow(T x,T n){ll ans=1;while(n--){ans*=x;}return ans;}
+
+#define rep(i,n) for(ll i=0;i<(ll)n;i++)
+#define rep2(i, begin_i, end_i) for (ll i = (ll)begin_i; i < (ll)end_i; i++)
+
+long long INF = 1LL<<60;
+ll solve(ll h){
+    if(h==1)return 1;
+    else {
+        return 2*solve(h/2)+1;
+    }
+}
+int main(){
+    ll h;
+    cin>>h;
+    ll ans=solve(h);
+    cout<<ans<<endl;
+    return 0;
+}
+
+
