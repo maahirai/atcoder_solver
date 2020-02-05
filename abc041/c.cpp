e709f8df13b2c4000ab2e96e8749ecd565d01816
musuchika <musu.tec@gmail.com>
2020-02-05 19:40:02 +0900

    はーい

---
+++ abc041/c.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef  long long ll;
+template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b;return true;}return false;}
+template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b;return true;}return false;}
+
+template<class T> ll llpow(T x,T n){ll ans=1;while(n--){ans*=x;}return ans;}
+template<class T> inline T gcd(T x,T y){if(y==0)return x; else {return gcd(y,x%y);}}
+template<class T> inline T lcm(T x,T y){return x/gcd(x,y)*y;}
+
+#define rep(i,n) for(ll i=0;i<(ll)n;i++)
+#define rep2(i, begin_i, end_i) for (ll i = (ll)begin_i; i < (ll)end_i; i++)
+
+long long INF = 1LL<<60;
+int main(){
+    int n;
+    cin>>n;
+    using pii=pair<int,int> ;
+    priority_queue<pii> p;
+    rep(i,n){
+        int h;
+        cin>>h;
+        p.emplace(h,i+1);
+    }
+    rep(i,n){
+       cout<<p.top().second<<endl;
+       p.pop();
+    }
+    return 0;
+}
+
+
