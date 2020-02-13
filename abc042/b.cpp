755617a16a95a3c6a3affc9bb8fd8c5777e50f9e
musuchika <musu.tec@gmail.com>
2020-02-13 14:21:36 +0900

    らーめんすっき おーれgit add b.cpp

---
+++ abc042/b.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef  long long ll;
+template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b;return true;}return false;}
+template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b;return true;}return false;}
+
+template<class T> ll llpow(T x,T n){ll ans=1;if(x==0)ans=0;while(n){if(n&1)ans*=x;x*=x;n>>=1;}return ans;}
+template<class T> inline T gcd(T x,T y){if(y==0)return x; else {return gcd(y,x%y);}}
+template<class T> inline T lcm(T x,T y){return x/gcd(x,y)*y;}
+
+#define rep(i,n) for(ll i=0;i<(ll)n;i++)
+#define rep2(i, begin_i, end_i) for (ll i = (ll)begin_i; i < (ll)end_i; i++)
+
+long long INF = 1LL<<60;
+int main(){
+    int n,l;
+    cin>>n>>l;
+    vector<string> s(n);
+    rep(i,n)cin>>s[i];
+    sort(s.begin(),s.end());
+    rep(i,n)cout<<s[i]; cout<<endl;
+    return 0;
+}
+
+
