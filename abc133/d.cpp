bd3628d665a234b9e18931b3e39e0dc49383b072
musuchika <musu.tec@gmail.com>
2020-04-05 16:37:43 +0900

    にゃ

---
+++ abc133/d.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef  long long ll;
+using vll=vector<ll>;
+using vi=vector<int>;
+using vvi=vector<vector<int>>;
+using vb=vector<bool>;
+using pii=pair<int,int>;
+
+template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b;return true;}return false;}
+template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b;return true;}return false;}
+
+//pow(llpow,modpow)
+template<class T> long long llpow(long long x,T n){long long ans=1;if(x==0)ans=0;while(n){if(n&1)ans*=x;x*=x;n>>=1;}return ans;}
+long long modpow(long long a, long long n, long long mod) {long long res = 1;while (n > 0) {if (n & 1) res = res * a % mod;a = a * a % mod;n >>= 1;}return res;}
+//最大公約数
+template<class T> inline T gcd(T x,T y){if(y==0)return x; else {return gcd(y,x%y);}}
+//最小公倍数
+template<class T> inline T lcm(T x,T y){return x/gcd(x,y)*y;}
+//逆元
+long long modinv(long long a, long long m) {long long b = m, u = 1, v = 0;while (b) {long long t = a / b;a -= t * b; swap(a, b);u -= t * v; swap(u, v);}u %= m;if (u < 0) u += m;return u;}
+
+#define rep(i, begin_i, end_i) for (ll i = (ll)begin_i; i < (ll)end_i; i++)
+
+long long INF = 1LL<<60;
+int main( ){
+    ll n;
+    cin>>n;
+    ll sum=0;
+    vll dum(n); rep(i,0,n)cin>>dum[i];
+    rep(i,0,n)sum+=dum[i];
+    ll sumrm0=0;
+    rep(i,0,n){
+        if(i%2)sumrm0+=2*dum[i];
+    }
+    vll res(n,0);
+    res[0]=sum-sumrm0;
+    rep(i,0,n-1){
+        res[i+1]=2*dum[i]-res[i];
+    }
+    rep(i,0,n)cout<<res[i]<<" ";
+    cout<<endl;
+    return 0;
+}
+
+
