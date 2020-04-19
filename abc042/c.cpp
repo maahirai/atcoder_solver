a47b278012325f2e6e9135a53d11d844f96cadb3
musuchika <musu.tec@gmail.com>
2020-04-19 17:15:50 +0900

    てい

--- abc042/c.cpp
+++ abc042/c.cpp
@@ -1,46 +1,57 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef  long long ll;
+using vll=vector<ll>;
+using vvll=vector<vll>;
+using vi=vector<int>;
+using vvi=vector<vector<int>>;
+using vb=vector<bool>;
+using pii=pair<int,int>;
+using vpii=vector<pair<int,int>>;
+
 template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b;return true;}return false;}
 template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b;return true;}return false;}
 
-template<class T> ll llpow(T x,T n){ll ans=1;if(x==0)ans=0;while(n){if(n&1)ans*=x;x*=x;n>>=1;}return ans;}
+//pow(llpow,modpow)
+template<class T> ll llpow(ll x,T n){ll ans=1;if(x==0)ans=0;while(n){if(n&1)ans*=x;x*=x;n>>=1;}return ans;}
+long long modpow(long long a, long long n, long long mod) {long long res = 1;while (n > 0) {if (n & 1) res = res * a % mod;a = a * a % mod;n >>= 1;}return res;}
+//最大公約数
 template<class T> inline T gcd(T x,T y){if(y==0)return x; else {return gcd(y,x%y);}}
+//最小公倍数
 template<class T> inline T lcm(T x,T y){return x/gcd(x,y)*y;}
+//逆元
+long long modinv(long long a, long long m) {long long b = m, u = 1, v = 0;while (b) {long long t = a / b;a -= t * b; swap(a, b);u -= t * v; swap(u, v);}u %= m;if (u < 0) u += m;return u;}
 
-#define rep(i,n) for(ll i=0;i<(ll)n;i++)
-#define rep2(i, begin_i, end_i) for (ll i = (ll)begin_i; i < (ll)end_i; i++)
+#define rep(i, begin_i, end_i) for (ll i = (ll)begin_i; i < (ll)end_i; i++)
+//試験導入
+#define irep(i, end_i, begin_i) for (ll i = (ll)begin_i-1; i >= (ll)end_i; i--)
 
 long long INF = 1LL<<60;
-
-int bfs(bool x[],int to){
-    static queue<int> q;
-    for(int i=1;i<10;i++){
-        if(x[i])q.push(i);
-    }
-    while(!q.empty()){
-       if(q.front()<to){
-        for(int j=0;j<10;j++){
-            if(x[j])q.push(q.front()*10+j);
-           }
-            q.pop();
-        }
-        else {
-            return q.front();
-        }
-    }
-}
-void solve(bool x[],int to){
-    int ans=bfs(x,to);
-    cout<<ans<<endl;
-}
-int main(){
+int main( ){
     int n,k;
     cin>>n>>k;
-    bool d[10];
-    rep(i,10)d[i]=true;
-    rep(i,k){int tmp;cin>>tmp;d[tmp]=false;}
-    solve(d,n);
+    vb canuse(10,true);
+    rep(i,0,k){
+        int dis;
+        cin>>dis;
+        canuse[dis]=false;
+    }
+    queue<int> q;
+    q.push(0);
+    while(!q.empty()){
+        int v=q.front();
+        q.pop();
+        rep(d,0,10){
+            if(canuse[d]){
+                int pv=v*10+d;
+                if(pv>=n){
+                    cout<<pv<<endl;
+                    return 0;
+                }
+                if(pv!=v)q.push(pv);
+            }
+        }
+    }
     return 0;
 }
 
