12933540d8bc4e97948409920da5093560c07708
musuchika <musu.tec@gmail.com>
2020-04-19 15:59:02 +0900

    ときなおし

--- abc129/d.cpp
+++ abc129/d.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 typedef  long long ll;
 using vll=vector<ll>;
+using vvll=vector<vll>;
 using vi=vector<int>;
 using vvi=vector<vector<int>>;
 using vb=vector<bool>;
 using pii=pair<int,int>;
-using vpii=vector<pii>;
+using vpii=vector<pair<int,int>>;
 
 template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b;return true;}return false;}
 template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b;return true;}return false;}
@@ -22,73 +23,63 @@ template<class T> inline T lcm(T x,T y){return x/gcd(x,y)*y;}
 long long modinv(long long a, long long m) {long long b = m, u = 1, v = 0;while (b) {long long t = a / b;a -= t * b; swap(a, b);u -= t * v; swap(u, v);}u %= m;if (u < 0) u += m;return u;}
 
 #define rep(i, begin_i, end_i) for (ll i = (ll)begin_i; i < (ll)end_i; i++)
-#define irep(i, end_i, begin_i) for (ll i = (ll)(begin_i-1); i >= (ll)end_i; i--)
+//試験導入
+#define irep(i, end_i, begin_i) for (ll i = (ll)begin_i-1; i >= (ll)end_i; i--)
 
 long long INF = 1LL<<60;
 int main( ){
-    int h,w; cin>>h>>w;
-    string s[h];
-    rep(i,0,h)cin>>s[i];
-    vector<vi> l(h,vi(w,0));
-    vector<vi> u(h,vi(w,0));
-    vector<vi> d(h,vi(w,0));
-    vector<vi> r(h,vi(w,0));
-    rep(i,0,h){
-        rep(j,0,w){
-            if(j==0){
-                if(s[i][j]=='.')l[i][j]++;
-            }
-            else{
-                if(s[i][j]=='#')continue;
-                else{
-                    l[i][j]=l[i][j-1]+1;
-                }
-            }
-        }
-    }
+    int h,w;
+    cin>>h>>w;
+    string S[h];
+    rep(i,0,h)cin>>S[i];
+    vvi r(h,vi(w,0));
+    vvi l(h,vi(w,0));
+    vvi u(h,vi(w,0));
+    vvi d(h,vi(w,0));
     rep(i,0,h){
         irep(j,0,w){
             if(j==w-1){
-                if(s[i][j]=='.')r[i][j]++;
+                if(S[i][j]=='.')r[i][j]=1;
             }
             else{
-                if(s[i][j]=='#')continue;
-                else{
-                    r[i][j]=r[i][j+1]+1;
-                }
+                if(S[i][j]=='.')r[i][j]=r[i][j+1]+1;
             }
         }
     }
-    rep(i,0,w){
-        irep(j,0,h){
-            if(j==h-1){
-                if(s[j][i]=='.')d[j][i]++;
-            }
-            else{
-                if(s[j][i]=='#')continue;
-                else{
-                    d[j][i]=d[j+1][i]+1;
-                }
-            }
-        }
-    }
-    rep(i,0,w){
-        rep(j,0,h){
+    rep(i,0,h){
+        rep(j,0,w){
             if(j==0){
-                if(s[j][i]=='.')u[j][i]++;
+                if(S[i][j]=='.')l[i][j]=1;
             }
             else{
-                if(s[j][i]=='#')continue;
-                else{
-                    u[j][i]=u[j-1][i]+1;
-                }
+                if(S[i][j]=='.')l[i][j]=l[i][j-1]+1;
+            }
+        }
+    }
+    rep(j,0,w){
+        irep(i,0,h){
+            if(i==h-1){
+                if(S[i][j]=='.')d[i][j]=1;
+            }
+            else{
+                if(S[i][j]=='.')d[i][j]=d[i+1][j]+1;
+            }
+        }
+    }
+    rep(j,0,w){
+        rep(i,0,h){
+            if(i==0){
+                if(S[i][j]=='.')u[i][j]=1;
+            }
+            else{
+                if(S[i][j]=='.')u[i][j]=u[i-1][j]+1;
             }
         }
     }
     int res=0;
     rep(i,0,h){
         rep(j,0,w){
-            chmax(res,u[i][j]+d[i][j]+l[i][j]+r[i][j]-3);
+            chmax(res,max(r[i][j]+l[i][j]+d[i][j]+u[i][j]-3,0));
         }
     }
     cout<<res<<endl;
