316726782be3c1751796692ad58366c58ee732c8
musuchika <musu.tec@gmail.com>
2020-04-20 12:12:41 +0900

    解き直し

--- abc140/d.cpp
+++ abc140/d.cpp
@@ -2,13 +2,18 @@
 using namespace std;
 typedef  long long ll;
 using vll=vector<ll>;
+using vvll=vector<vll>;
 using vi=vector<int>;
+using vvi=vector<vector<int>>;
+using vb=vector<bool>;
+using pii=pair<int,int>;
+using vpii=vector<pair<int,int>>;
 
 template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b;return true;}return false;}
 template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b;return true;}return false;}
 
 //pow(llpow,modpow)
-template<class T> ll llpow(T x,T n){ll ans=1;if(x==0)ans=0;while(n){if(n&1)ans*=x;x*=x;n>>=1;}return ans;}
+template<class T> ll llpow(ll x,T n){ll ans=1;if(x==0)ans=0;while(n){if(n&1)ans*=x;x*=x;n>>=1;}return ans;}
 long long modpow(long long a, long long n, long long mod) {long long res = 1;while (n > 0) {if (n & 1) res = res * a % mod;a = a * a % mod;n >>= 1;}return res;}
 //最大公約数
 template<class T> inline T gcd(T x,T y){if(y==0)return x; else {return gcd(y,x%y);}}
@@ -17,19 +22,22 @@ template<class T> inline T lcm(T x,T y){return x/gcd(x,y)*y;}
 //逆元
 long long modinv(long long a, long long m) {long long b = m, u = 1, v = 0;while (b) {long long t = a / b;a -= t * b; swap(a, b);u -= t * v; swap(u, v);}u %= m;if (u < 0) u += m;return u;}
 
-#define rep(i,n) for(ll i=0;i<(ll)n;i++)
-#define rep2(i, begin_i, end_i) for (ll i = (ll)begin_i; i < (ll)end_i; i++)
+#define rep(i, begin_i, end_i) for (ll i = (ll)begin_i; i < (ll)end_i; i++)
+//試験導入
+#define irep(i, end_i, begin_i) for (ll i = (ll)begin_i-1; i >= (ll)end_i; i--)
 
 long long INF = 1LL<<60;
-int main(){
-    int n,k;
-    cin>>n>>k;
+int main( ){
+    int n;
+    cin>>n;
+    int k;
+    cin>>k;
     string s;
     cin>>s;
-    int score=0;
-    rep(i,n-1)if(s[i]==s[i+1])score++;
-    int ans=min(n-1,score+2*k);
-    cout<<ans<<endl;
+    int happy=0;
+    rep(i,0,n-1)if(s[i]==s[i+1])happy++;
+
+    cout<<min(n-1,2*k+happy)<<endl;
     return 0;
 }
 
