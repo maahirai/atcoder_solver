7d56c9f92d3b6de42d8791d52d9f35a080e0843c
musuchika <musu.tec@gmail.com>
2020-04-20 11:57:37 +0900

    解き直し

--- abc113/c.cpp
+++ abc113/c.cpp
@@ -1,35 +1,57 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef  long long ll;
+using vll=vector<ll>;
+using vvll=vector<vll>;
+using vi=vector<int>;
+using vvi=vector<vector<int>>;
+using vb=vector<bool>;
+using pii=pair<int,int>;
+using vpii=vector<pair<int,int>>;
+
 template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b;return true;}return false;}
 template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b;return true;}return false;}
 
-template<class T> ll llpow(T x,T n){ll ans=1;if(x==0)ans=0;while(n){if(n&1)ans*=x;x*=x;n>>=1;}return ans;}
+//pow(llpow,modpow)
+template<class T> ll llpow(ll x,T n){ll ans=1;if(x==0)ans=0;while(n){if(n&1)ans*=x;x*=x;n>>=1;}return ans;}
+long long modpow(long long a, long long n, long long mod) {long long res = 1;while (n > 0) {if (n & 1) res = res * a % mod;a = a * a % mod;n >>= 1;}return res;}
+//最大公約数
 template<class T> inline T gcd(T x,T y){if(y==0)return x; else {return gcd(y,x%y);}}
+//最小公倍数
 template<class T> inline T lcm(T x,T y){return x/gcd(x,y)*y;}
+//逆元
+long long modinv(long long a, long long m) {long long b = m, u = 1, v = 0;while (b) {long long t = a / b;a -= t * b; swap(a, b);u -= t * v; swap(u, v);}u %= m;if (u < 0) u += m;return u;}
 
-#define rep(i,n) for(ll i=0;i<(ll)n;i++)
-#define rep2(i, begin_i, end_i) for (ll i = (ll)begin_i; i < (ll)end_i; i++)
+#define rep(i, begin_i, end_i) for (ll i = (ll)begin_i; i < (ll)end_i; i++)
+//試験導入
+#define irep(i, end_i, begin_i) for (ll i = (ll)begin_i-1; i >= (ll)end_i; i--)
 
 long long INF = 1LL<<60;
-int main(){
-    using pii=pair<int,int>;
-    int n,m;
-    cin>>n>>m;
-    vector<int> manage[n];
-    vector<pii> pf(m);
-    rep(i,m){
-        int y,p;
+int main( ){
+    int n;
+    cin>>n;
+    int m;
+    cin>>m;
+    vpii pref[n];
+    vpii ans(m);
+    rep(i,0,m){
+        int p,y;
         cin>>p>>y;
-        p--;y--;
-        manage[p].push_back(y);
-        pf[i]=make_pair(p,y);
+        p--;
+        pii t=make_pair(y,i);
+        pref[p].push_back(t);
     }
-    for(int i=0;i<n;i++){
-        sort(manage[i].begin(),manage[i].end());
+    rep(i,0,n)sort(pref[i].begin(),pref[i].end());
+    rep(i,0,n){
+        int cnt=0;
+        for(pii t:pref[i]){
+            cnt++;
+            pii in=make_pair(i+1,cnt);
+            ans[t.second]=in;
+        }
     }
-    for(int i=0;i<m;i++){
-        printf("%06d%06d\n",pf[i].first+1,lower_bound(manage[pf[i].first].begin(),manage[pf[i].first].end(),pf[i].second)-manage[pf[i].first].begin()+1);
+    rep(i,0,m){
+        printf("%06d%06d\n",ans[i].first,ans[i].second);
     }
     return 0;
 }
